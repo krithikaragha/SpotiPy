6d762b236f73cab8825dacc062db38303ce9c81c
Ritu Mishra <rmishra1990@gmail.com>
2019-04-13 13:55:04 -0400

    Final Commit

---
+++ queries.sql
@@ -0,0 +1,19 @@
+create database spotipy;
+
+use spotipy;
+
+create table artists(
+	artist varchar(50), 
+    `# of followers` long,
+    `# of shows` long,
+    `recent album` varchar(50),
+    danceability float,
+    energy float,
+    loudness float,
+    speechiness float,
+    valence float); 
+
+select * from artist;    
+    
+    
+    
